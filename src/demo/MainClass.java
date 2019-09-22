c92a6d8e848f99942519ef37bd70fbf39db28d1d
Толянчик <tolyara1961@gmail.com>
2019-09-23 00:56:48 +0300

    commit

--- src/demo/MainClass.java
+++ src/demo/MainClass.java
@@ -5,16 +5,20 @@ import java.io.IOException;
 import java.io.PrintStream;
 
 import service.MyOut;
+import storages.DataStorage;
+import storages.MyORM;
 
 public class MainClass {
 
 	private static final String POSTGRESQL_DRIVER = "org.postgresql.Driver";
 	private static final String VERSION = "beta version";
 	private static final PrintStream myOut = MyOut.getInstance().getOut();
+	private static final MyORM myORM = new MyORM();
 
 	public static void main(String[] args) throws IOException {
 
 		printHeader();
+		myORM.createTable("testTable");
 
 	}
 
--- src/storages/MyORM.java
+++ src/storages/MyORM.java
@@ -7,45 +7,55 @@ import java.sql.SQLException;
 
 import service.Settings;
 
-public class MyORM {
-	
+public class MyORM implements DataStorage {
+
 	private Connection connection;
-	
+
 	private static final String QUERY_CREATE_DATABASE = "CREATE DATABASE ?;";
 	private static final String QUERY_CREATE_TABLE = "CREATE TABLE ?;";
 	private static final String QUERY_DROP_AND_CREATE_DATABASE = "DROP DATABASE ?;" + "CREATE DATABASE ?;";
 	private static final String QUERY_DROP_AND_CREATE_TABLE = "DROP TABLE ?;" + "CREATE TABLE ?;";
-	
-	
-	public MyORM(String driver) {
-		final Settings settings = Settings.getInstance();
+
+	public MyORM() {
+		// final Settings settings = Settings.getInstance();
 		try {
-			Class.forName(driver);
-			this.connection = DriverManager.getConnection(settings.getValue("postgres.url"), settings.getValue("postgres.username"),
-					settings.getValue("postgres.password"));
+			Class.forName("org.postgresql.Driver");
+			this.connection = DriverManager.getConnection("jdbc:postgresql://127.0.0.1:5432/MyORM", "postgres",
+					"123321");
 		} catch (SQLException e) {
 			throw new IllegalStateException(e);
 		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
+	public MyORM(String driver) {
+		final Settings settings = Settings.getInstance();
+		try {
+			Class.forName(driver);
+			this.connection = DriverManager.getConnection(settings.getValue("postgres.url"),
+					settings.getValue("postgres.username"), settings.getValue("postgres.password"));
+		} catch (SQLException e) {
+			throw new IllegalStateException(e);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
 	public void createDatabase(String nameDB) {
 		try (final PreparedStatement statement = this.connection.prepareStatement(QUERY_CREATE_DATABASE)) {
 			statement.setString(1, nameDB);
 			statement.executeUpdate();
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void createTable(String nameTable) {
 		try (final PreparedStatement statement = this.connection.prepareStatement(QUERY_CREATE_TABLE)) {
 			statement.setString(1, nameTable);
 			statement.executeUpdate();
-		}
-		catch (SQLException e) {
+		} catch (SQLException e) {
 			e.printStackTrace();
 		}
 	}
