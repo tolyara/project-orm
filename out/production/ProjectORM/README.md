3b439fba993bb753db92e6bbdc33773b0a8780a1
zhe1ezniak <dimazhel88@gmail.com>
2019-10-08 16:21:39 +0300

    commit

--- out/production/ProjectORM/README.md
+++
@@ -1,5 +0,0 @@
-Project ORM 
-
-features in dev (only PostgreSQL):
-- create/delete tables
-- CRUD data to/from table
--- out/production/ProjectORM/scripts/createTableWorker.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE `myorm`.`worker` (
-  `worker_id` INT NOT NULL,
-  `worker_surname` VARCHAR(45) NULL,
-  `worker_address` INT NULL,
-  `worker_salary` DOUBLE NULL,
-  PRIMARY KEY (`worker_id`));
